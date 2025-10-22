d631d4e2a0d2591dae2cb4230ed97d692aa43ca9
cysof <cp4reallife@yahoo.com>
2025-10-23 00:20:22 +0100

    fix api file

--- src/api.js
+++ src/api.js
@@ -1,24 +1,29 @@
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
-// export const BASE_URL = 'http://127.0.0.1:8000/';
-export const API_BASE_URL = 'https://allstarfashion-yb2ng.sevalla.app';
+// Base URL for API
+export const BASE_URL = 'https://allstarfashion-yb2ng.sevalla.app';
 
+// Create axios instance
 const api = axios.create({
   baseURL: BASE_URL,
 });
 
+// Add token to requests if available and valid
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('access');
+
     if (token) {
       const decoded = jwtDecode(token);
-      const expiry_date = decoded.exp;
-      const current_time = Date.now() / 1000;
-      if (expiry_date > current_time) {
+      const expiryDate = decoded.exp;
+      const currentTime = Date.now() / 1000;
+
+      if (expiryDate > currentTime) {
         config.headers.Authorization = `Bearer ${token}`;
       }
     }
+
     return config;
   },
   (error) => {
