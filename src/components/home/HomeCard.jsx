136f6fc29d332dd1a88d68709a017a93406c4e7b
cysof <cp4reallife@yahoo.com>
2025-10-26 01:51:17 +0100

    update: Homecard to fix cloudinary image URLs

--- src/components/home/HomeCard.jsx
+++ src/components/home/HomeCard.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaShoppingBag } from 'react-icons/fa';
 import styles from './HomeCard.module.css';
-import { BASE_URL } from '../../api';
 
 const HomeCard = ({ product }) => {
   return (
@@ -14,9 +13,18 @@ const HomeCard = ({ product }) => {
               <FaShoppingBag className={styles.placeholderIcon} />
               <span>
                 <img
-                  src={`${BASE_URL}${product.image}`}
+                  src={
+                    product.medium_image_url ||
+                    product.thumbnail_url ||
+                    product.image_url ||
+                    'https://dummyimage.com/300x300/dee2e6/6c757d.jpg'
+                  }
                   className={styles.cardImgTop}
-                  alt="Product Image"
+                  alt={product.name || 'Product Image'}
+                  onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = 'https://dummyimage.com/300x300/dee2e6/6c757d.jpg';
+                  }}
                 />
               </span>
             </div>
@@ -25,9 +33,9 @@ const HomeCard = ({ product }) => {
             </div>
           </div>
           <div className={styles.cardBody}>
-            <h5 className={styles.cardTitle}> {product.name}</h5>
+            <h5 className={styles.cardTitle}>{product.name}</h5>
             <div className={styles.priceContainer}>
-              <h6 className={styles.cardPrice}> {product.price}</h6>
+              <h6 className={styles.cardPrice}>${product.price}</h6>
               <span className={styles.discountBadge}>-20%</span>
             </div>
             <div className={styles.rating}>
